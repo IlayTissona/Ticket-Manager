af4c89052fe65bd3b93ad7cf665fd7e2af03e31e
IlayTissona <ilay211@gmail.com>
2021-03-23 21:05:19 +0200

    added axios GET request

--- client/src/components/TicketList.js
+++ client/src/components/TicketList.js
@@ -1,12 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Ticket from "./Ticket";
 
 function TicketList(props) {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/tickets").then((res) => {
+      setList(res.data);
+      console.log(res);
+    });
+  }, []);
+
   return (
     <div className="tickets-component">
       <div className="search-area"></div>
       <ul className="ticket-list">
-        <Ticket />
+        {list
+          .filter((ticket) => !ticket.hidden)
+          .map((ticketObj) => {
+            return <Ticket ticket={ticketObj} />;
+          })}
       </ul>
     </div>
   );
