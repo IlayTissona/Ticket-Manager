920a5b8bcfc5de1219a062da5f2219d0befe03df
IlayTissona <ilay211@gmail.com>
2021-03-23 19:19:37 +0200

    created ticket schema

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const app = express();
 
 app.use(express.static("client/build"));
 
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "client/build/index.html");
+});
+
 module.exports = app;
--- seedDB.js
+++ seedDB.js
@@ -1,3 +1,15 @@
 const seed = require("./seeder");
-
 seed(process.env.MONGO_URI);
+
+const mongoose = require("mongoose");
+
+const ticketSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  userEmail: { type: String, required: true },
+  done: Boolean,
+  creationTime: { type: Date, required: true },
+  labels: [String],
+});
+
+module.exports = mongoose.model("Ticket", ticketSchema);
