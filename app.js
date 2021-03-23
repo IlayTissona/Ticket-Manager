9da08089177922fc7816e79b29a29bef0dc15705
IlayTissona <ilay211@gmail.com>
2021-03-23 19:22:29 +0200

    first get endpoint

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const Ticket = require("./seedDB");
 
 app.use(express.static("client/build"));
 
@@ -7,4 +8,7 @@ app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "client/build/index.html");
 });
 
+app.get("/api/tickets", (req, res) => {
+  Ticket.find({}).then((result) => res.json(result));
+});
 module.exports = app;
